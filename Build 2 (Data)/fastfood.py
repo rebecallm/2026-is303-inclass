caf058a5cfba4bca7bb4946783c8541d4db577c4
Rebeca Llontop <rl388964@byu.edu>
2026-05-06 09:28:52 -0600

    activity for error

---
+++ Build 2 (Data)/fastfood.py
@@ -0,0 +1,10 @@
+name = "justin"
+food = "s"
+
+if food and name: 
+    form_complete = True 
+
+if form_complete: 
+    print(f"{name}'s favorite food is {food}")
+else: 
+    print("please fill out the form completely")
