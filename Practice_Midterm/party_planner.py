f4c02feb55aafe03da1b417662a552d876b9b126
Rebeca Llontop <rl388964@byu.edu>
2026-06-01 08:58:06 -0600

    Practiced midterm

---
+++ Practice_Midterm/party_planner.py
@@ -0,0 +1,59 @@
+# Write a program that helps plan a party by calculating costs. The program should:
+# 1.	Ask the user for:
+# •	Number of guests (a whole number)
+# •	Cost per person for food (a number)
+# •	Cost per person for drinks (a number)
+# •	Whether they need to rent a venue: yes or no (a string)
+num_guests =int(input("How many number of guests? "))
+food_cost = float(input("What is the cost per person for food? "))
+drink_cost = float(input("What is the cost per person for drinks? "))
+venue_cost = input("Do you need to rent a venue? yes or no ").lower()
+if venue_cost == "yes": 
+    venue_price = 250 
+else: 
+    venue_price = 0 
+# 2.	Calculate:
+# •	Food total (guests × food cost per person)
+# •	Drink total (guests × drink cost per person)
+# •	Venue cost: $0 if they do not need a venue, $250.00 if they do
+# •	Grand total (food + drinks + venue)
+food_total = num_guests * food_cost
+drink_total = num_guests * drink_cost
+venue_total = venue_price
+grand_total = (food_total + drink_total + venue_total)
+# 3.	Print a formatted summary using f-strings with costs formatted to 2 decimal places:
+# --- Party Cost Summary ---
+# Guests: 20
+# Food total: $300.00
+# Drink total: $100.00
+# Venue cost: $250.00
+# Grand total: $650.00
+# 4.	If the grand total exceeds $500, print: "Tip: Ask guests to bring a dish to reduce costs!"
+print(f"--- Party Cost Summary ---\n"
+      f"Guests: {num_guests:.2f}\n"
+      f"Food total: {food_total:.2f}\n"
+      f"Drink total: {drink_total:.2f}\n"
+      f"Venue cost: {venue_total:.2f}\n"
+      f"Grand total: {grand_total:.2f}\n")
+
+if grand_total > 500:
+    print(f"Tip: Ask guests to bring a dish to reduce costs!")
+# Constraints
+# •	You must use at least one if/else (for the venue check)
+# •	You must use f-strings for all output
+# •	Handle "yes", "Yes", "YES", etc. (case-insensitive)
+
+# Example Run
+# Number of guests: 20
+# Food cost per person: $15.00
+# Drink cost per person: $5.00
+# Do you need to rent a venue? (yes/no): Yes
+
+# --- Party Cost Summary ---
+# Guests: 20
+# Food total: $300.00
+# Drink total: $100.00
+# Venue cost: $250.00
+# Grand total: $650.00
+
+# Tip: Ask guests to bring a dish to reduce costs!
